a37c84ecf2f8d318e9d931d9e7f877166ccc13b4
Temch4k <artsiom.skarakhod@gmail.com>
2021-03-15 21:29:44 -0600

    part2 done

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -8,17 +8,20 @@ app.component('book-display', {
     },
     template:
         /*html*/
-        `<ul class="col-sm-12 col-md-6 col-xl-4">
-            <div v-if="bookObj.volumeInfo.title != undefined">
-                <a class="list-group-item list-group-item-action active" href="this.bookObj.selfLink"> {{ this.bookObj.volumeInfo.title }}</a>
-            </div>
-            <div v-if="bookObj.volumeInfo.authors != undefined">
-                <li class="list-group-item"> {{ this.bookObj.volumeInfo.authors }}</li>
-            </div>
-            <div  v-if="bookObj.volumeInfo.publisher != undefined">
-                <li class="list-group-item"> {{ this.bookObj.volumeInfo.publisher }}, {{ this.bookObj.volumeInfo.publishedDate }}</li>
-            </div>
-        </ul>`,
+    `<ul class="col-sm-12 col-md-6 col-xl-4">
+        <div v-if="bookObj.volumeInfo.title != undefined">
+            <a class="list-group-item list-group-item-action active" :href="this.bookObj.selfLink"> {{ this.bookObj.volumeInfo.title }}</a>
+        </div>
+        <div v-if="bookObj.volumeInfo.authors != undefined">
+            <li class="list-group-item" v-for="author in this.bookObj.volumeInfo.authors"> Author: {{ author }} </li>
+        </div>
+        <div  v-if="bookObj.volumeInfo.publisher != undefined">
+            <li class="list-group-item"> Publisher: {{ this.bookObj.volumeInfo.publisher }}</li>
+        </div>
+        <div  v-if="bookObj.volumeInfo.publisher != undefined">
+            <li class="list-group-item"> Published Date: {{ this.bookObj.volumeInfo.publishedDate }}</li>
+        </div>
+    </ul>,`,
     computed:{
         bookObj(){
             if(this.book != null)
