a6b1df4922cdea1e4420a6b97663e092dd6c6d06
Temch4k <artsiom.skarakhod@gmail.com>
2021-03-15 20:09:28 -0600

    part 2 pt1

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -9,9 +9,15 @@ app.component('book-display', {
     template:
         /*html*/
         `<ul class="col-sm-12 col-md-6 col-xl-4">
-        <a class="list-group-item list-group-item-action active" href="this.bookObj.selfLink"> {{ this.bookObj.volumeInfo.title }}</a>
-        <li class="list-group-item"> {{ this.bookObj.volumeInfo.authors }}</li>
-        <li class="list-group-item"> {{ this.bookObj.volumeInfo.publisher }}, {{ this.bookObj.volumeInfo.publishedDate }}</li>
+            <div v-if="bookObj.volumeInfo.title != undefined">
+                <a class="list-group-item list-group-item-action active" href="this.bookObj.selfLink"> {{ this.bookObj.volumeInfo.title }}</a>
+            </div>
+            <div v-if="bookObj.volumeInfo.authors != undefined">
+                <li class="list-group-item"> {{ this.bookObj.volumeInfo.authors }}</li>
+            </div>
+            <div  v-if="bookObj.volumeInfo.publisher != undefined">
+                <li class="list-group-item"> {{ this.bookObj.volumeInfo.publisher }}, {{ this.bookObj.volumeInfo.publishedDate }}</li>
+            </div>
         </ul>`,
     computed:{
         bookObj(){
